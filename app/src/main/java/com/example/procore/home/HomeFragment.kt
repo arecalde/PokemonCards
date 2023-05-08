56417c4a70638fe4f9c4d783bc2aaabbfc00bb12
arecalde <arecalde@ucdavis.edu>
2023-05-08 12:01:41 -0700

    interview finished

--- app/src/main/java/com/example/procore/home/HomeFragment.kt
+++ app/src/main/java/com/example/procore/home/HomeFragment.kt
@@ -2,19 +2,21 @@ package com.example.procore.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.procore.R
 import com.example.procore.databinding.FragmentHomeBinding
 import com.example.procore.helper.ItemAdapter
+import com.google.android.material.chip.Chip
 
 class HomeFragment : Fragment() {
     private val viewModel: HomeViewModel by viewModels()
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,12 +24,39 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+        var itemAdapter = ItemAdapter(emptyList())
 
-        viewModel.pokemonCards.observe(viewLifecycleOwner) {
-            binding.pokemonCardGrid.adapter = ItemAdapter(it)
+        viewModel.filteredPokemonCards.observe(viewLifecycleOwner) { pokemonList ->
+            itemAdapter = ItemAdapter(pokemonList)
+            binding.pokemonCardGrid.adapter = itemAdapter
             binding.pokemonCardGrid.layoutManager = GridLayoutManager(requireContext(), 4)
         }
 
+        binding.chipGroupFilter.setOnCheckedStateChangeListener { group, checkedIds ->
+            viewModel.setChosenType(checkedIds.first())
+        }
+
+        viewModel.pokemonTypes.observe(viewLifecycleOwner) {
+            it.forEach {type ->
+
+                binding.chipGroupFilter.addView(
+                    createChip(type)
+                )
+            }
+
+        }
+
+
         return binding.root
     }
+
+    private fun createChip(chipTitle: String) = Chip(context).apply {
+            text = chipTitle
+            isChipIconVisible = false
+            isCloseIconVisible = false
+
+            // necessary to get single selection working
+            isClickable = true
+            isCheckable = true
+        }
 }
--- app/src/main/java/com/example/procore/home/HomeViewModel.kt
+++ app/src/main/java/com/example/procore/home/HomeViewModel.kt
@@ -5,28 +5,60 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.procore.helper.ItemAdapter
 import com.example.procore.network.RetrofitHelper
 import com.example.procore.network.json.Pokemon
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
-    private val _pokemonCards = MutableLiveData<List<Pokemon>>()
-    val pokemonCards: LiveData<List<Pokemon>>
-        get() = _pokemonCards
+    private val _pokemonTypes = MutableLiveData(setOf<String>())
+    val pokemonTypes: LiveData<Set<String>>
+        get() = _pokemonTypes
+
+    private var pokemonCards: List<Pokemon>? = null
+
+    private val _filteredPokemonCards = MutableLiveData<List<Pokemon>>()
+    val filteredPokemonCards: LiveData<List<Pokemon>>
+        get() = _filteredPokemonCards
 
     private val _loading = MutableLiveData(false)
     val loading: LiveData<Boolean>
         get() = _loading
 
+    private val _chosenType = MutableLiveData<Int>()
+    val chosenType: LiveData<Int>
+        get() = _chosenType
+
     init {
         viewModelScope.launch {
             _loading.value = true
-            _pokemonCards.value = RetrofitHelper.getPokemonCards()?.data?.toList()
+            pokemonCards = RetrofitHelper.getPokemonCards()?.data?.toList()
+            _filteredPokemonCards.value = pokemonCards ?: return@launch
+            val setOfTypes = mutableSetOf<String>()
+            pokemonCards?.forEach { pokemon ->
+                pokemon.types.forEach { type ->
+                    setOfTypes.add(type)
+                }
+            }
+            _pokemonTypes.value = setOfTypes
+
             _loading.value = false
         }
     }
 
+    fun setChosenType(newType: Int) {
+        _chosenType.value = newType
+        val setOfTypes = pokemonTypes.value ?: return
+        val type = setOfTypes.toList()[newType-1]
+        pokemonCards?.let { pokemonCards ->
+            val filteredCards =
+                pokemonCards.filter {
+                    it.types.contains(type)
+                }
+            _filteredPokemonCards.value = filteredCards
+        }
+    }
     fun sortCardsByType() {
-        _pokemonCards.value = _pokemonCards.value?.sortedBy { it.types.first() }
+        _filteredPokemonCards.value = pokemonCards?.sortedBy { it.types.first() }
     }
 }
